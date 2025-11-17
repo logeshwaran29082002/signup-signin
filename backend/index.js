a4cf1cfd805075a9f7f790c3045bca8715b5abf1
Logeshwaran <logeshwaran2982.com>
2025-11-17 12:29:31 +0530

    Schema-finesh

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const db = require('./db')
+const router = require('./routes/userRoutes');
 
+//Middleware
+app.use(express.json());
 
+// router middleware
 
+app.use('/',router)
 
 app.listen(3000,()=>{
     console.log('Server is Running on the port 3000')
