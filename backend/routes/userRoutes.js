b9c6c47ba3401aede156766ace85b41cf8fdf5be
Logeshwaran <logeshwaran2982.com>
2025-11-17 14:13:43 +0530

    post and get  route is fiinesh

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userdata = require('../models/userSignupData')
-
-
+const userdata = require("../models/userSignupData");
 
 //post router
 
-router.post('/',async(req , res)=>{
+router.post("/signup", async (req, res) => {
+  try {
     const postdata = new userdata(req.body);
     const posted = await postdata.save();
-    res.status(201).json(posted)
+    res.status(201).json(posted);
+  } catch (err) {
+    res.status(404).json("postError", err);
+  }
+});
+// get All Data router
+router.get("/", async (req, res) => {
+  const getData = await userdata.find();
+  res.status(201).json(getData);
+});
+
+
+// get Single Data Router
+
+router.get('/:id',async(req,res)=>{
+    const getSingleData = await userdata.findById(req.params.id)
+    res.status(201).json(getSingleData)
 })
 
-
-
-module.exports = router
+module.exports = router;
